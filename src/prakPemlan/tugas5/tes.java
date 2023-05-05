78ad171d9b8b59e24346c1a693b88406b234167a
Theoraditya <theoraditya195@gmail.com>
2023-05-05 13:29:40 +0700

    tes

--- src/prakPemlan/tugas5/tes.java
+++ src/prakPemlan/tugas5/tes.java
@@ -4,5 +4,6 @@ public class tes {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("yes");
+        System.out.println("tes");
     }
 }
